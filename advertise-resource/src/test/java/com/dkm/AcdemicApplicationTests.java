d0ab97891721790bd7c6a8a941ef192eab1ce075
vincent <vincent@eqlee.com>
2020-07-23 13:47:23 +0800

    'qf'

---
+++ advertise-resource/src/test/java/com/dkm/AcdemicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcdemicApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
